bcd6ea2fcfbe0d648b1a1ae529823b06df58eee3
lokeshjammi <jammilokesh3@gmail.com>
2024-09-19 22:58:37 +0530

    View Saved Users code commit

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ app = Flask(__name__)
 Bootstrap5(app)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
 base_file = os.path.dirname(__file__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+os.path.join(base_file, "instance/today_update.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(base_file, "instance/today_update.db")
 
 db = SQLAlchemy(app)
+
+
+#==============Database Tables================
 
 
 class Todays(db.Model):
@@ -30,9 +33,16 @@ class Todays(db.Model):
     bugs_verified = db.Column(type_=db.String, name="bugs_verified")
 
 
+class Users(db.Model):
+    user_id = db.Column(type_=db.Integer, name="user_id", primary_key=True, autoincrement=True)
+    user_name = db.Column(type_=db.String, name="user_name", unique=True)
+    user_email = db.Column(type_=db.String, name="user_email_id", unique=True)
+
+
+#==============Database Tables================
+
 with app.app_context():
     db.create_all()
-    db.session.rollback()
 
 
 class AddWorkingTickets(FlaskForm):
@@ -48,23 +58,55 @@ class AddWorkingTickets(FlaskForm):
     submit = SubmitField(label="Submit")
 
 
+class AddUsers(FlaskForm):
+    new_qa_name = StringField(label="Name", validators=[DataRequired()])
+    new_qa_email = EmailField(label="Email", validators=[DataRequired()])
+    submit = SubmitField(label="Submit")
+
+
 @app.route("/")
 def homepage():
     db.session.rollback()
     return render_template('index.html')
 
 
-@app.route("/add", methods=['POST', 'GET'])
+@app.route("/add_user", methods=['POST', 'GET'])
+def add_users():
+    db.session.rollback()
+    add_new_user = AddUsers()
+    try:
+        if add_new_user.validate_on_submit():
+            query = db.session.query(Users).count()
+            print(query)
+            new_user = Users(user_id=query + 1, user_name=request.form.get('new_qa_name'),
+                             user_email=request.form.get('new_qa_email'))
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for(endpoint='homepage'))
+    except Exception as e:
+        print(e)
+    return render_template('add_user.html', add_new_user=add_new_user)
+
+
+@app.route("/add_ticket", methods=['POST', 'GET'])
 def add_work_tickets():
     add_working_tickets = AddWorkingTickets()
     if add_working_tickets.validate_on_submit():
         query = db.session.query(Todays).count()
-        new_status = Todays(id=query+1, name=request.form.get('qa_name'), email=request.form.get('qa_email'), sprint_id=request.form.get('qa_sprint_id'),
-                            story_id=request.form.get('qa_story_id'),)
+        new_status = Todays(id=query + 1, name=request.form.get('qa_name'), email=request.form.get('qa_email'),
+                            sprint_id=request.form.get('qa_sprint_id'),
+                            story_id=request.form.get('qa_story_id'), )
         db.session.add(new_status)
         db.session.commit()
         return redirect(url_for('homepage'))
-    return render_template('add.html', add=add_working_tickets)
+    return render_template('add_tickets.html', add=add_working_tickets)
+
+
+@app.route("/view_users", methods=['POST', 'GET'])
+def view_registered_users():
+    users_data = db.session.query(Users).all()
+    print(users_data)
+    return render_template('view_users.html', users_data=users_data)
 
 
 @app.route("/update")
